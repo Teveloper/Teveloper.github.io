61956668f093c04b7fd4dd4ffb5244ef9c4e5394
Jake Park <5965265+Teveloper@users.noreply.github.com>
2019-01-09 10:36:38 +0900

    Create myCalculator.py

---
+++ myCalculator.py
@@ -0,0 +1,13 @@
+# pythonTDD_practice
+# myCalculatoir.py
+def add(a, b):
+    return a + b
+ 
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
