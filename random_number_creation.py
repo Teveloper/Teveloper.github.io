73fd3d39c8999c1a58a9af76793ab1c3b133c4ea
Jake Park <5965265+Teveloper@users.noreply.github.com>
2019-01-11 09:32:49 +0900

    Create random_number_creation.py

---
+++ random_number_creation.py
@@ -0,0 +1,35 @@
+import random
+
+#generate random numbers
+def randomNumbers():
+    randomNumList = []
+    while len(randomNumList) < 10:
+        num = random.randint(-100, 100)
+        if num not in randomNumList:
+            randomNumList.append(num)
+    
+    randomNumList.sort()
+    #print(randomNumList)
+    return randomNumList
+
+#positive.py
+def positive(l):
+    result = []
+    for i in l:
+        if i > 0:
+            result.append(i)
+    return result
+
+#negative.py
+def negative(l):
+    result = []
+    for i in l:
+        if i < 0:
+            result.append(i)
+    return result
+
+#print all result
+randomNumerResult = randomNumbers()
+print(randomNumerResult)
+print("그 중 양수는 : ", positive(randomNumerResult))
+print("그 중 음수는 : ", negative(randomNumerResult))
