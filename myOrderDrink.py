42bc6176b4c3a6719eda3e92dc5a15cb02554b1f
김만수 <40420814+mansoo-of-oz@users.noreply.github.com>
2019-01-11 17:20:48 +0900

    코드 최적화
    다음과 같이 코드를 수정하면 어떨까요?

--- myOrderDrink.py
+++ myOrderDrink.py
@@ -1,36 +1,48 @@
+drink_list = ["커피", "홍차", "녹차"]
+drink_price_list = [5000, 4000, 3000]
+error_message = [
+    "주문을 알아 듣지 못했습니다.",
+    "최소 한 잔 이상을 주문해야 합니다.",
+    "예측할 수 없는 오류 발생",
+]
+
 def askingOrder():
     typeOfDrink = input("커피, 홍차, 녹차가 있습니다. 마실 음료수를 입력하세요 : ")
         
-    if typeOfDrink == "커피" or typeOfDrink == "홍차" or typeOfDrink == "녹차":
-        askingNumberOfCups(typeOfDrink)
-    else:
-        print("주문을 알아듣지 못했습니다.")
-        askingOrder()
+    if not typeOfDrink in drink_list:
+        return False, typeOfDrink
 
-def askingNumberOfCups(typeOfDrink):
-    numberOfCups = input("몇 잔 주문하시겠어요?")
+    return True, typeOfDrink
 
-    try:
-        print("감사합니다. ", typeOfDrink, "를 ", int(numberOfCups), "잔 드리겠습니다.")
-        drinkOrderAndCalculate(typeOfDrink, numberOfCups)
-    except ValueError:
-        print("정수가 아닙니다.")
-        askingNumberOfCups(typeOfDrink)
+def askingNumberOfCups():
+    numberOfCups = int(input("몇 잔 주문하시겠어요?"))
+    
+    return numberOfCups
 
 def drinkOrderAndCalculate (typeOfDrink, numberOfCups):
-    priceCoffee = 5000
-    priceGreenTea = 4000
-    priceRedTea = 3000
-    
-    if typeOfDrink == "커피":
-        totalPrice = int(priceCoffee) * int(numberOfCups)
-    elif typeOfDrink == "홍차":
-        totalPrice = int(priceGreenTea) * int(numberOfCups)
-    elif typeOfDrink == "녹차":
-        totalPrice = int(priceRedTea) * int(numberOfCups)
-    else:
+    if not typeOfDrink in drink_list:
         print("예측할 수 없는 오류 발생")
+        return False, 0
+        
+    index = drink_list.index(typeOfDrink)
+    totalPrice = drink_price_list[index] * int(numberOfCups)
     
-    print(typeOfDrink, numberOfCups, "잔 드리겠습니다. 금액은 ", totalPrice, "원입니다.")
+    return True, totalPrice
 
-askingOrder()
+result, typeOfDrink = askingOrder()
+error_message_type = 0
+
+if result == True:
+    numberOfCups = askingNumberOfCups()
+    
+    error_message_type = 1
+    if numberOfCups > 0:
+        result, totalPrice = drinkOrderAndCalculate(typeOfDrink, numberOfCups)
+        
+        error_message_type = 2
+        if result == True:
+            error_message_type = -1
+            print("감사합니다. {}를 {}잔 드리겠습니다. 금액은 {} 원입니다.".format(typeOfDrink, numberOfCups, totalPrice))
+
+if error_message_type != -1:
+    print(error_message[error_message_type])
